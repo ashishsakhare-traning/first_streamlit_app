76b7da064cf1e264846f9aac7d1385661fda2b7d
ashishsakhare-traning <152278330+ashishsakhare-traning@users.noreply.github.com>
2023-12-11 06:49:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit 
 import pandas
 import requests
 import snowflake.connector
@@ -11,7 +11,7 @@ streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range EGG')
 streamlit.text('🥑🍞 Avocado Toast')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -20,7 +20,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
@@ -30,7 +30,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 
 
-st.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('what fruit would you like information about?')
   if not fruit_choice:
@@ -39,7 +39,7 @@ try:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.datafram(back_from_function)
 
-st.header("The fruit load list contains: ")
+streamlit.header("The fruit load list contains: ")
 
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
